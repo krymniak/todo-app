b4031c2dfc2226963693dbcdbaa261bbad7ff9ee
krymniak <krymnyak@gmail.com>
2022-10-27 12:40:46 +0300

    create todos route

--- src/app/todos/todos.module.ts
+++ src/app/todos/todos.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 import { TodosComponent } from "./components/todos/todos.component";
+
+const routes: Routes = [
+	{
+		path: '',
+		component: TodosComponent
+	}
+]
 
 @NgModule({
 	declarations:[
 		TodosComponent
 	],
-	imports:[]
+	imports:[RouterModule.forChild(routes)]
 })
 
 export class TodosModule {
