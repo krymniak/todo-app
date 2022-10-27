8483019acb0dcce9ed3cf15abeb9f1581a9cc261
krymniak <krymnyak@gmail.com>
2022-10-27 12:32:42 +0300

    creating todos component and module

---
+++ src/app/todos/todos.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from "@angular/core";
+import { TodosComponent } from "./components/todos/todos.component";
+
+@NgModule({
+	declarations:[
+		TodosComponent
+	],
+	imports:[]
+})
+
+export class TodosModule {
+
+}
